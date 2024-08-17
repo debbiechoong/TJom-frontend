16eb5d0ba2460e6b9b5999dc160857ddfe3e812c
zianttt <ziant0927@gmail.com>
2024-08-18 02:14:41 +0800

    update: ocr

--- lib/modules/food/ocr.dart
+++ lib/modules/food/ocr.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
-import 'package:lottie/lottie.dart';
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -15,22 +14,36 @@ class MenuOCRPage extends StatefulWidget {
 
 class _MenuOCRPageState extends State<MenuOCRPage> {
   final TextEditingController _textController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
   File? _selectedImage;
   String? _ocrText;
   String? _llmResponse;
-  Offset _floatingButtonPos = Offset(300, 50);
-  bool _showNotes = false;
   bool _isLoading = false;
+  bool _showPrompt = false;
 
-  List<Map<String, String>> _messages = [];
+  List<Map<String, String?>> _messages = [];
+  final int _historyThreshold = 5;
 
-  final Map<String, bool> _checkedSentences = {
-    '일 인분 주세요. (Il-een-boon Ju-se-yo) - Please give me one serving.': false,
-    '김치 주세요 (Igeo juseyo) - Please give me Kimchi': false,
-    '계산해주세요 (Gyesanhaejuseyo) - Please calculate (the bill)': false,
-  };
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _pickImage();
+    });
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   Future<void> _pickImage() async {
+    setState(() {
+      _showPrompt = true;
+    });
+
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
@@ -44,10 +57,18 @@ class _MenuOCRPageState extends State<MenuOCRPage> {
                 Navigator.pop(context);
                 final pickedImage =
                     await ImagePicker().getImage(source: ImageSource.camera);
+                setState(() {
+                  _showPrompt = false;
+                });
                 if (pickedImage != null) {
                   setState(() {
                     _selectedImage = File(pickedImage.path);
                     _isLoading = true;
+                    _messages.add({
+                      'role': 'user',
+                      'content': null,
+                      'image': _selectedImage!.path,
+                    });
                   });
                   await _callOCR();
                   setState(() {
@@ -63,10 +84,18 @@ class _MenuOCRPageState extends State<MenuOCRPage> {
                 Navigator.pop(context);
                 final pickedImage =
                     await ImagePicker().getImage(source: ImageSource.gallery);
+                setState(() {
+                  _showPrompt = false;
+                });
                 if (pickedImage != null) {
                   setState(() {
                     _selectedImage = File(pickedImage.path);
                     _isLoading = true;
+                    _messages.add({
+                      'role': 'user',
+                      'content': null,
+                      'image': _selectedImage!.path,
+                    });
                   });
                   await _callOCR();
                   setState(() {
@@ -98,8 +127,8 @@ class _MenuOCRPageState extends State<MenuOCRPage> {
     final response = await request.send();
 
     if (response.statusCode == 200) {
-      final responseData = await http.Response.fromStream(response);
-      final responseBody = json.decode(responseData.body);
+      final responseData = utf8.decode(await response.stream.toBytes());
+      final responseBody = json.decode(responseData);
 
       if (responseBody is Map<String, dynamic> &&
           responseBody.containsKey('text')) {
@@ -107,16 +136,16 @@ class _MenuOCRPageState extends State<MenuOCRPage> {
         setState(() {
           _ocrText = extractedText;
           _messages.removeWhere((msg) => msg['content'] == 'loading...');
-          _messages.add({'role': 'user', 'content': extractedText});
+          _messages.add({'role': 'system', 'content': extractedText});
         });
-        await _callLLM(extractedText);
+        _scrollToBottom();
       } else {
-        // TODO: Error handling
         setState(() {
           _messages.removeWhere((msg) => msg['content'] == 'loading...');
           _messages
               .add({'role': 'system', 'content': 'Error: Incorrect response.'});
         });
+        _scrollToBottom();
       }
     } else {
       print('OCR failed: ${response.statusCode}');
@@ -124,10 +153,11 @@ class _MenuOCRPageState extends State<MenuOCRPage> {
         _messages.removeWhere((msg) => msg['content'] == 'loading...');
         _messages.add({'role': 'system', 'content': 'Error occurred.'});
       });
+      _scrollToBottom();
     }
   }
 
-  Future<void> _callLLM(String text) async {
+  Future<void> _callLLM(String userPrompt) async {
     setState(() {
       _messages.add({'role': 'system', 'content': 'loading...'});
     });
@@ -139,11 +169,28 @@ class _MenuOCRPageState extends State<MenuOCRPage> {
       'Content-Type': 'application/json',
     };
 
+    List<Map<String, String?>> recentMessages =
+        _messages.length > _historyThreshold
+            ? _messages.sublist(_messages.length - _historyThreshold)
+            : _messages;
+
+    String formattedMessages = recentMessages.map((msg) {
+      final role = msg['role'] == 'user' ? 'User' : 'Assistant';
+      return '$role: ${msg['content']}';
+    }).join('\n');
+
+    final systemPrompt = '''
+  You are a helpful assistant.
+  Menu: ${_ocrText ?? "No menu available"}
+  Recent messages:
+  $formattedMessages
+  ''';
+
     final body = json.encode({
       'model': 'solar-1-mini-chat',
       'messages': [
-        {'role': 'system', 'content': 'You are a helpful assistant.'},
-        {'role': 'user', 'content': text},
+        {'role': 'system', 'content': systemPrompt},
+        {'role': 'user', 'content': userPrompt},
       ],
       'stream': false,
     });
@@ -151,15 +198,17 @@ class _MenuOCRPageState extends State<MenuOCRPage> {
     final response = await http.post(url, headers: headers, body: body);
 
     if (response.statusCode == 200) {
-      final responseData = json.decode(response.body);
+      final responseData = utf8.decode(response.bodyBytes);
+      final responseBody = json.decode(responseData);
 
-      if (responseData is Map<String, dynamic> &&
-          responseData.containsKey('choices')) {
-        final llmMessage = responseData['choices'][0]['message']['content'];
+      if (responseBody is Map<String, dynamic> &&
+          responseBody.containsKey('choices')) {
+        final llmMessage = responseBody['choices'][0]['message']['content'];
         setState(() {
           _messages.removeWhere((msg) => msg['content'] == 'loading...');
           _messages.add({'role': 'system', 'content': llmMessage});
         });
+        _scrollToBottom();
       } else {
         print('Unexpected response structure');
         setState(() {
@@ -169,6 +218,7 @@ class _MenuOCRPageState extends State<MenuOCRPage> {
             'content': 'Error: Unexpected response structure'
           });
         });
+        _scrollToBottom();
       }
     } else {
       print('LLM error: ${response.statusCode}');
@@ -176,14 +226,16 @@ class _MenuOCRPageState extends State<MenuOCRPage> {
         _messages.removeWhere((msg) => msg['content'] == 'loading...');
         _messages.add({'role': 'system', 'content': 'Error occurred.'});
       });
+      _scrollToBottom();
     }
   }
 
-  void _handleCheckboxToggle(String sentence) {
-    setState(() {
-      _checkedSentences[sentence] = !_checkedSentences[sentence]!;
-    });
-    _showBalloonAnimation();
+  void _scrollToBottom() {
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
   }
 
   void _handleSubmit() {
@@ -197,12 +249,21 @@ class _MenuOCRPageState extends State<MenuOCRPage> {
     _callLLM(_textController.text);
 
     _textController.clear();
+
+    _scrollToBottom();
   }
 
-  void _toggleKoreanSentences() {
-    setState(() {
-      _showNotes = !_showNotes;
-    });
+  void _handleOrder() {
+    if (_ocrText != null) {
+      _callLLM(
+          'Generate 3 sentences to teach foreigners how to order food from the menu. Each sentence should be in the following format: Korean (Romanized) - English. For example, 아이스 아메리카노 한 잔 주세요 (Aiseu Amerikano han jan juseyo) - One iced Americano, please.');
+    } else {
+      setState(() {
+        _messages
+            .add({'role': 'system', 'content': 'Error: No menu available.'});
+      });
+      _scrollToBottom();
+    }
   }
 
   @override
@@ -232,6 +293,7 @@ class _MenuOCRPageState extends State<MenuOCRPage> {
                 ),
                 Expanded(
                   child: ListView.builder(
+                    controller: _scrollController,
                     padding: const EdgeInsets.symmetric(horizontal: 16.0),
                     itemCount: _messages.length,
                     itemBuilder: (context, index) {
@@ -245,13 +307,23 @@ class _MenuOCRPageState extends State<MenuOCRPage> {
                           margin: const EdgeInsets.symmetric(vertical: 5),
                           padding: const EdgeInsets.all(12),
                           decoration: BoxDecoration(
-                            color: Color(0xFFA6E84E),
+                            color:
+                                isUser ? Color(0xFFA6E84E) : Colors.grey[300],
                             borderRadius: BorderRadius.circular(12),
                           ),
-                          child: Text(
-                            message['content']!,
-                            style: TextStyle(color: Colors.black),
-                          ),
+                          child: message['image'] != null
+                              ? Image.file(
+                                  File(message['image']!),
+                                  width: 200, // Adjust size as needed
+                                  fit: BoxFit.cover,
+                                )
+                              : Text(
+                                  message['content']!,
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontFamily: 'Roboto',
+                                  ),
+                                ),
                         ),
                       );
                     },
@@ -259,132 +331,78 @@ class _MenuOCRPageState extends State<MenuOCRPage> {
                 ),
                 Padding(
                   padding: const EdgeInsets.all(16.0),
-                  child: Row(
+                  child: Column(
                     children: [
-                      Expanded(
-                        child: TextField(
-                          controller: _textController,
-                          decoration: InputDecoration(
-                            labelText: 'Talk to Jejom',
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Color(0xFFFAF120),
-                              ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Color(0xFFFAF120),
+                      ActionChip(
+                        label: Text('Order'),
+                        onPressed: _handleOrder,
+                        backgroundColor: Color(0xFFA6E84E),
+                        labelStyle: TextStyle(color: Colors.black),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              controller: _textController,
+                              decoration: InputDecoration(
+                                labelText: 'Talk to Jejom',
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Color(0xFFFAF120),
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Color(0xFFFAF120),
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.send),
-                        color: Colors.white,
-                        onPressed: _handleSubmit,
-                      ),
-                      GestureDetector(
-                        onTap: _pickImage,
-                        child: CircleAvatar(
-                          backgroundColor: Color(0xFFFAF120),
-                          child: Icon(Icons.photo_camera, color: Colors.black),
-                        ),
+                          IconButton(
+                            icon: Icon(Icons.send),
+                            color: Colors.white,
+                            onPressed: _handleSubmit,
+                          ),
+                          GestureDetector(
+                            onTap: _pickImage,
+                            child: CircleAvatar(
+                              backgroundColor: Color(0xFFFAF120),
+                              child:
+                                  Icon(Icons.photo_camera, color: Colors.black),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
                 ),
               ],
             ),
-            Positioned(
-              left: _floatingButtonPos.dx,
-              top: _floatingButtonPos.dy,
-              child: Draggable(
-                feedback: FloatingActionButton(
-                  onPressed: _toggleKoreanSentences,
-                  child: Icon(Icons.chat),
-                  backgroundColor: Color(0xFFFAF120),
-                ),
-                childWhenDragging: Container(),
-                onDraggableCanceled: (Velocity velocity, Offset offset) {
-                  setState(() {
-                    _floatingButtonPos = offset;
-                  });
-                },
-                child: FloatingActionButton(
-                  onPressed: _toggleKoreanSentences,
-                  child: Icon(Icons.chat),
-                  backgroundColor: Color(0xFFFAF120),
+            if (_showPrompt)
+              Center(
+                child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.7),
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  child: const Text(
+                    "Take a picture of the menu",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Roboto',
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ),
-            ),
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
-      bottomSheet: _showNotes ? _buildKoreanSentencesOverlay() : null,
     );
-  }
-
-  Widget _buildKoreanSentencesOverlay() {
-    return Stack(
-      children: [
-        Opacity(
-          opacity: 0.5,
-          child: ModalBarrier(dismissible: false, color: Colors.black),
-        ),
-        Center(
-          child: Container(
-            margin: EdgeInsets.all(20),
-            padding: EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              color: Colors.grey[850],
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: _checkedSentences.keys.map((sentence) {
-                return ListTile(
-                  title: Text(sentence, style: TextStyle(color: Colors.white)),
-                  trailing: GestureDetector(
-                    onTap: () => _handleCheckboxToggle(sentence),
-                    child: _checkedSentences[sentence]!
-                        ? Icon(Icons.check, color: Colors.green)
-                        : Checkbox(
-                            value: _checkedSentences[sentence],
-                            onChanged: (bool? value) {
-                              _handleCheckboxToggle(sentence);
-                            },
-                          ),
-                  ),
-                );
-              }).toList(),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  void _showBalloonAnimation() {
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) => Center(
-        child: Lottie.asset(
-          'assets/balloon_fireworks.json',
-          width: 200,
-          height: 200,
-          repeat: false,
-          onLoaded: (composition) {
-            Future.delayed(composition.duration, () {
-              Navigator.of(context).pop();
-            });
-          },
-        ),
-      ),
-    ).then((_) {
-      _toggleKoreanSentences();
-    });
   }
 }
