4223263f25913dfe77c75529d61b1e54a09a33c1
zianttt <ziant0927@gmail.com>
2024-08-18 19:38:24 +0800

    feat: allergy checker and translation

--- lib/modules/food/menu.dart
+++ lib/modules/food/menu.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:jejom/providers/user_provider.dart';
+import 'package:jejom/models/user.dart';
 
 class MealPreferences extends StatefulWidget {
-  const MealPreferences({super.key});
+  final VoidCallback onPreferencesUpdated;
+  const MealPreferences({required this.onPreferencesUpdated, super.key});
 
   @override
   _MealPreferencesState createState() => _MealPreferencesState();
 }
 
 class _MealPreferencesState extends State<MealPreferences> {
-  double _budgetLevel = 50;
-  final double _maxBudget = 100;
-
-  bool _includeBreakfast = false;
-  bool _includeLunch = false;
-  bool _includeDinner = false;
-
+  final TextEditingController _allergyController = TextEditingController();
   String _selectedFoodType = "everything";
 
   @override
@@ -37,105 +35,8 @@ class _MealPreferencesState extends State<MealPreferences> {
               ),
               const SizedBox(height: 16),
               const Text(
-                "Meals preferences",
+                "Food Preferences & Allergies",
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                "Select the meals you would like to include in your trip, your budget for it, and the type of food you prefer.",
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                "Budget level",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 5),
-              const Text(
-                "Depending on the budget level, we will select least or more expensive restaurants.",
-                style: TextStyle(fontSize: 14),
-              ),
-              Slider(
-                value: _budgetLevel,
-                min: 0,
-                max: _maxBudget,
-                divisions: 2,
-                label: _budgetLevel.round().toString(),
-                onChanged: (double value) {
-                  setState(() {
-                    _budgetLevel = value;
-                  });
-                },
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Cheap", style: TextStyle(fontSize: 14)),
-                  Text("Mid", style: TextStyle(fontSize: 14)),
-                  Text("High", style: TextStyle(fontSize: 14)),
-                ],
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                "Meals to include",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 5),
-              const Text(
-                "Prices are an estimate of each meal for 8 days 1 person",
-                style: TextStyle(fontSize: 14),
-              ),
-              const SizedBox(height: 10),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                margin: const EdgeInsets.symmetric(vertical: 5),
-                child: CheckboxListTile(
-                  value: _includeBreakfast,
-                  title: const Text("Breakfast"),
-                  subtitle: const Text("\$16\n(\$2 per meal)"),
-                  onChanged: (bool? value) {
-                    setState(() {
-                      _includeBreakfast = value ?? false;
-                    });
-                  },
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                margin: const EdgeInsets.symmetric(vertical: 5),
-                child: CheckboxListTile(
-                  value: _includeLunch,
-                  title: const Text("Lunch"),
-                  subtitle: const Text("\$29\n(\$4 per meal)"),
-                  onChanged: (bool? value) {
-                    setState(() {
-                      _includeLunch = value ?? false;
-                    });
-                  },
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                margin: const EdgeInsets.symmetric(vertical: 5),
-                child: CheckboxListTile(
-                  value: _includeDinner,
-                  title: const Text("Dinner"),
-                  subtitle: const Text("\$30\n(\$4 per meal)"),
-                  onChanged: (bool? value) {
-                    setState(() {
-                      _includeDinner = value ?? false;
-                    });
-                  },
-                ),
               ),
               const SizedBox(height: 20),
               const Text(
@@ -143,55 +44,18 @@ class _MealPreferencesState extends State<MealPreferences> {
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 10),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                margin: const EdgeInsets.symmetric(vertical: 5),
-                child: RadioListTile<String>(
-                  title: const Text("Everything"),
-                  value: "everything",
-                  groupValue: _selectedFoodType,
-                  onChanged: (String? value) {
-                    setState(() {
-                      _selectedFoodType = value!;
-                    });
-                  },
-                ),
+              ..._buildFoodTypeRadioButtons(),
+              const SizedBox(height: 20),
+              const Text(
+                "Allergens",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                margin: const EdgeInsets.symmetric(vertical: 5),
-                child: RadioListTile<String>(
-                  title: const Text("Vegetarian"),
-                  value: "vegetarian",
-                  groupValue: _selectedFoodType,
-                  onChanged: (String? value) {
-                    setState(() {
-                      _selectedFoodType = value!;
-                    });
-                  },
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                margin: const EdgeInsets.symmetric(vertical: 5),
-                child: RadioListTile<String>(
-                  title: const Text("Vegan"),
-                  value: "vegan",
-                  groupValue: _selectedFoodType,
-                  onChanged: (String? value) {
-                    setState(() {
-                      _selectedFoodType = value!;
-                    });
-                  },
+              const SizedBox(height: 10),
+              TextField(
+                controller: _allergyController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter your allergens (separated by commas)',
                 ),
               ),
               const SizedBox(height: 20),
@@ -211,15 +75,43 @@ class _MealPreferencesState extends State<MealPreferences> {
     );
   }
 
-  void _submitPreferences() {
-    Map<String, dynamic> preferences = {
-      'budgetLevel': _budgetLevel,
-      'includeBreakfast': _includeBreakfast,
-      'includeLunch': _includeLunch,
-      'includeDinner': _includeDinner,
-      'selectedFoodType': _selectedFoodType,
-    };
+  List<Widget> _buildFoodTypeRadioButtons() {
+    return [
+      _buildFoodTypeRadioButton("Everything"),
+      _buildFoodTypeRadioButton("Vegetarian"),
+      _buildFoodTypeRadioButton("Vegan"),
+    ];
+  }
 
-    print("User Preferences: $preferences");
+  Widget _buildFoodTypeRadioButton(String value) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      child: RadioListTile<String>(
+        title: Text(value),
+        value: value.toLowerCase(),
+        groupValue: _selectedFoodType,
+        onChanged: (String? value) {
+          setState(() {
+            _selectedFoodType = value!;
+          });
+        },
+      ),
+    );
+  }
+
+  void _submitPreferences() {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    final allergies =
+        _allergyController.text.split(',').map((e) => e.trim()).toList();
+
+    userProvider.updateUserAllergies(allergies);
+    userProvider.updateUserDietary(_selectedFoodType);
+
+    Navigator.pop(context);
+    widget.onPreferencesUpdated();
   }
 }
